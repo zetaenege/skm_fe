5c775cc8855bc58270571e20c46c7abec8285794
zetaenege <jonka@wtrlmn.com>
2025-06-14 17:40:23 +0200

    forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
 import NavBar from "./components/common/navBAr/NavBar.jsx";
 import Dashboard from "./pages/dashboard/Dashboard.jsx";
+import Login from "./components/formSteps/Login.jsx";
+import ForgotPassword from "./components/formSteps/ForgotPassword.jsx";
 
 function App() {
 
     return (
         <>
             <div className="container">
-                <NavBar/>
-                <Dashboard/>
+               <ForgotPassword/>
             </div>
         </>
     )
