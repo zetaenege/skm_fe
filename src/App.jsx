361c15bf7f3fb72ea29006cb89fb29433de99939
zetaenege <jonka@wtrlmn.com>
2025-06-14 21:08:05 +0200

    basic pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import NavBar from "./components/common/navBAr/NavBar.jsx";
 import Dashboard from "./pages/dashboard/Dashboard.jsx";
 import Login from "./components/formSteps/Login.jsx";
 import ForgotPassword from "./components/formSteps/ForgotPassword.jsx";
+import CreatedNewPassword from "./components/formSteps/CreateNewPassword.jsx";
+import SignUp from "./components/formSteps/SignUp.jsx";
+import AccountCreated from "./components/formSteps/AccountCreated.jsx";
 
 function App() {
 
     return (
         <>
             <div className="container">
-               <ForgotPassword/>
+               <AccountCreated />
             </div>
         </>
     )
--- src/components/formSteps/ForgotPassword.jsx
+++ src/components/formSteps/ForgotPassword.jsx
@@ -7,9 +7,11 @@ function ForgotPassword() {
 
         <section className={styles.centered__container}>
             <form className={styles.login__form}>
-                <h2 className={styles.form__title}>Welcome!</h2>
+                <h2 className={styles.form__title}>Forgot password</h2>
+                <p>Please enter your <b>email address</b> to recieve a verification link.</p>
 
                 <div>
+                    <label className={styles.form__label} htmlFor="email">Email address</label>
                     <input
                         type="email"
                         id="email"
@@ -20,26 +22,11 @@ function ForgotPassword() {
                     />
                 </div>
 
-                <div>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        placeholder="Enter your password"
-                        className={styles.form__input}
-                        required
-                    />
-                </div>
 
-                <p><a href="#" className="info__text">Forgot password?</a></p>
 
-                <Button type="submit">Sign in</Button>
+                <Button type="submit">Send</Button>
 
-                <div className={styles.link__wrapper}>
-                    <p className="info__text">
-                        Not a member? <a href="#" className={styles.link}>Register now</a>
-                    </p>
-                </div>
+
             </form>
         </section>
     );
